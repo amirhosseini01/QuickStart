0b908debbb60eaf706466b89623390aa2f88170f
Amir <amircsharp7@gmail.com>
2024-01-16 22:22:59 +0330

    feat: upgrade client proj to dotnet 8 and change folders structure

---
+++ src/Web/Client/Program.cs
@@ -0,0 +1,19 @@
+using Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Web.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient<ApiClient>(client =>
+{
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+
+    // The cookie auth stack detects this header and avoids redirects for unauthenticated
+    // requests
+    client.DefaultRequestHeaders.TryAddWithoutValidation("X-Requested-With", "XMLHttpRequest");
+});
+
+await builder.Build().RunAsync();
