483b8c0693745f1b9f2bb3efa8bac972fdb9ec52
Amir <amircsharp7@gmail.com>
2024-02-22 16:44:47 +0330

    fix: migrations: re-run Identity migration and make Identity migration class name capital

--- src/Api/Modules/Product/Repositories/Implementations/ProductRepository.cs
+++ src/Api/Modules/Product/Repositories/Implementations/ProductRepository.cs
@@ -3,15 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Api.Modules.Product;
 
-public class ProductRepository: GenericRepository<Product>, IProductRepository
-{
-    private readonly DbSet<Product> _entities;
-	public ProductRepository(ApiDbContext context) : base(context) => _entities = context.Products;
+// public class ProductRepository: GenericRepository<Product>, IProductRepository
+// {
+//     private readonly DbSet<Product> _entities;
+// 	public ProductRepository(ApiDbContext context) : base(context) => _entities = context.Products;
 
-	public async Task<List<ProductListVm>> GetProductLists(ProductListFilter filter, CancellationToken cancellationToken = default) 
-    {
-        var query = _entities.AsNoTracking();
+// 	public async Task<List<ProductListVm>> GetProductLists(ProductListFilter filter, CancellationToken cancellationToken = default) 
+//     {
+//         var query = _entities.AsNoTracking();
 
-        return await query.MapProductList().ToListAsync(cancellationToken);
-    }
-}
+//         return await query.MapProductList().ToListAsync(cancellationToken);
+//     }
+// }
